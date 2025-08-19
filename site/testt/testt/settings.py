f6c4db4acff8e4deb2a40dba55f51fc13333adc1
mouadrguibi <146367397+mouad4949@users.noreply.github.com>
2025-08-19 22:16:18 +0100

    Update settings.py

--- site/testt/testt/settings.py
+++ site/testt/testt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)*$oa23_9#m^zz3d+4)vi^_0jiu0flasegs-tj)2+sjtwpdl^_'
+SECRET_KEY = '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,8 +33,8 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER='mouadrguibi900@gmail.com'
-EMAIL_HOST_PASSWORD="obsiedwhesduzvhx"
-DEFAULT_FROM_EMAIL="AEROCLUB <mouadrguibi900@gmail.com>"
+EMAIL_HOST_PASSWORD="$$$$$$$$$$$$$$$"
+DEFAULT_FROM_EMAIL="AEROCLUB <$$$$$$$$$$>"
 ALLOWED_HOSTS = []
 
 
